2a19d143584c95b97ab9fe90c9b17ee2efba2922
krumdine <turonjeremy@gmail.com>
2014-11-27 17:56:05 +0100

    mst

--- src/Algorithmic.cpp
+++ src/Algorithmic.cpp
@@ -51,14 +51,16 @@ vector<int> bruteForce(vector<vector<int> > graph){
 		int j = (i % 2 == 1) ? p[i] : 0;
 		swap(&solution, i+1, j+1);
 
+		int tmp = poids(graph, solution);
+		++cpt ;
+
 		time_t t2 ;
 		time(&t2) ;
 		if(difftime(t2,t) >= 1.) {
 			t = t2 ;
-			printf("%d/%ld\n",++cpt,NbPerm) ;
+			printf("%d/%ld\n",cpt,NbPerm) ;
 		}
 
-		int tmp = poids(graph, solution);
 		if (tmp < min){
 			min = tmp;
 			bestSol = solution ;
@@ -71,4 +73,69 @@ vector<int> bruteForce(vector<vector<int> > graph){
 	}
 	return bestSol ;
 }
-//le cycle commence du sommet zero
+
+int edgeWeight(vector<int> edge, vector<vector<int> > graph) {
+	return graph[edge[0]][edge[1]] ;
+}
+
+int partitionner(vector<vector<int> > *edges, int begin, int end, int pivot, vector<vector<int> > graph) {
+	vector<int> tmp = (*edges)[pivot] ;
+	(*edges)[pivot] = (*edges)[end] ;
+	(*edges)[end] = tmp ;
+	int j = begin ;
+	for(int i = begin ; i < end - 1 ; i++) {
+		if( edgeWeight((*edges)[i],graph) <= edgeWeight((*edges)[end],graph) ) {
+			tmp = (*edges)[i] ;
+			(*edges)[i] = (*edges)[j] ;
+			(*edges)[j++] = tmp ;
+		}
+	}
+	tmp = (*edges)[end] ;
+	(*edges)[end] = (*edges)[j] ;
+	(*edges)[j] = tmp ;
+	return j ;
+}
+
+void quickSort(vector<vector<int> > *edges, int begin, int end, vector<vector<int> > graph) {
+	if(begin < end) {
+		int pivot = begin ;
+	    pivot = partitionner(edges, begin, end, pivot, graph) ;
+	    quickSort(edges ,begin, pivot-1, graph) ;
+	    quickSort(edges, pivot+1, end, graph) ;
+	}
+}
+
+vector<int> minimumSpanningTree(vector<vector<int> > graph) {
+	vector<vector<int> > mst ;
+	vector<vector<int> > edges ;
+	vector<int> component ;
+
+	//we put the edges in 'edges'
+	for(int i = 0 ; i < graph.size() ; i++) {
+		for(int j = i + 1 ; j < graph.size() ; j++) {
+			vector<int> currEdge ;
+			currEdge.push_back(i) ;
+			currEdge.push_back(j) ;
+			edges.push_back(currEdge) ;
+		}
+	}
+	//we sort 'edges'
+	quickSort(&edges, 0, edges.size(), graph) ;
+
+	//we initialize the component
+	for(int i = 0 ; i < graph.size() ; i++)
+		component.push_back(i) ;
+
+	int count = 0 ;
+	int i = 0 ;
+	while(count < (graph.size() - 1) && i < edges.size()) {
+		int u = edges[i][0] ;
+		int v = edges[i][1] ;
+		if(component[u] != component[v]) {
+			mst.push_back(edges[i]) ;
+			component[u] = component[v] ;
+		}
+	}
+
+	//find a hamiltonian path from the minimum spanning tree
+}
